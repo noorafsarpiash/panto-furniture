d2d72845cf4c5cbb4919cc94186ac096eefae9c8
Noor Piash <noorpiash1234@gmail.com>
2025-05-21 00:42:26 +0600

    background image done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const navItems = [
 
 const NavItems = ({ toggleMenu }) => {
     return (
-        <ul className='flex flex-col md:flex-row items-center md:space-x-8 gap-8 '>
+        <ul className='flex flex-col md:flex-row items-center md:space-x-8 gap-8  '>
             {navItems.map((item, index) => (
                 <li key={index} onClick={toggleMenu} >
                     <NavLink to={item.path} className={({ isActive }) =>
@@ -53,32 +53,34 @@ const Navbar = () => {
 
 
     return (
-        <nav className='max-w-screen-2xl  mx-auto py-6 px-4 flex justify-between items-center'>
-            {/* logo */}
-            <Link to="/" className='font-bold'>Logo</Link>
-            <div onClick={toggleMenu} className='md:hidden text-xl cursor-pointer hover:text-primary'>
-                {
-                    isMenuOpen ? null : <FaBars />
-                }
-            </div>
-
-            <div className='hidden md:flex'>
-                <NavItems />
-            </div>
-
-            <div className={`fixed top-0 left-0 w-full h-screen bg-black bg-opacity-80 flex flex-col items-center justify-center space-y-8 text-white transition-transform transform ${isMenuOpen ? "translate-x-0" : "-translate-x-full"} md:hidden `}>
-                <div className='absolute top-14 right-14 text-xl cursor-pointer' onClick={toggleMenu}>
-                    <FaTimes />
+        <header className={`fixed top-0 left-0 right-0 z-50 transition duration-300 ease-in-out text-white`}>
+            <nav className='max-w-screen-2xl container  mx-auto py-6 px-4 flex justify-between items-center'>
+                {/* logo */}
+                <Link to="/" className='font-bold'>Logo</Link>
+                <div onClick={toggleMenu} className='md:hidden text-xl cursor-pointer hover:text-primary'>
+                    {
+                        isMenuOpen ? null : <FaBars />
+                    }
                 </div>
-                <NavItems toggleMenu={toggleMenu} />
-            </div>
+
+                <div className='hidden md:flex'>
+                    <NavItems />
+                </div>
+
+                <div className={`fixed top-0 left-0 w-full h-screen bg-black bg-opacity-80 flex flex-col items-center justify-center space-y-8 text-white transition-transform transform ${isMenuOpen ? "translate-x-0" : "-translate-x-full"} md:hidden `}>
+                    <div className='absolute top-6 right-6 text-xl cursor-pointer' onClick={toggleMenu}>
+                        <FaTimes />
+                    </div>
+                    <NavItems toggleMenu={toggleMenu} />
+                </div>
 
 
-            <div className='hidden md:block cursor-pointer relative  '><FaShoppingBag className='text-xl' />
-                <sup className='absolute top-0 -right-3 bg-primary text-white w-5 h-5 rounded-full flex items-center justify-center text-xs'>0</sup>
-            </div>
+                <div className='hidden md:block cursor-pointer relative  '><FaShoppingBag className='text-xl' />
+                    <sup className='absolute top-0 -right-3 bg-primary text-white w-5 h-5 rounded-full flex items-center justify-center text-xs'>0</sup>
+                </div>
 
-        </nav>
+            </nav>
+        </header>
     )
 }
 
--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -3,7 +3,15 @@ import bannerImg from "../../assets/banner.png"
 
 const Hero = () => {
     return (
-        <div className='h-screen' style={{ backgroundImage: `url(${bannerImg})` }}></div>
+        <section className='h-screen relative bg-cover bg-center text-white' style={{ backgroundImage: `url(${bannerImg})` }}>
+
+            <div className=' px-2  md:pt-44 pt-24 text-center space-y-6 md:w-1/2 mx-auto'>
+                <h1 className='text-4xl lg:text-6xl font-medium lg:leading-tight leading-snug'>Make your interior more
+                    minimalistic & modern</h1>
+                <p className='text-2xl font-normal lg:w-1/2 mx-auto'>Turn your room with panto into a lot more minimalist and modern with ease and speed</p>
+            </div>
+
+        </section>
     )
 }
 
