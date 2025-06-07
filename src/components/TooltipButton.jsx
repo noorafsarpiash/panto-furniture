9a06f76117cb1e6194e489920f03951c23b1479a
Noor Piash <noorpiash1234@gmail.com>
2025-06-07 17:34:33 +0600

    tooltip

--- src/components/TooltipButton.jsx
+++ src/components/TooltipButton.jsx
@@ -1,20 +1,33 @@
 import React from 'react'
 
-const TooltipButton = () => {
+const TooltipButton = ({ position = "bottom", colors = ["bg-red-700", "bg-green-700", "bg-blue-700"] }) => {
+    const postionClasses = {
+        bottom: {
+            tooltip: "bottom-full left-1/2 transform -translate-x-1/2 mb-3",
+            arrow: "left-1/2 transform -translate-x-1/2 -bottom-2 border-1"
+
+        }
+
+    }
+
+
+
     return (
         <div className='relative group'>
-            <button className='relative p-4 bg-white/25 rounded-full border border-1 bg-white'>
-                <div className='hidden absolute space-x-1 bottom-full left-1/2 transform -translate-x-1/2 mb-3 p-2 rounded-lg bg-white/25 shadow-md group-hover:flex'>
-                    <div className='size-6 bg-red-700 border border-white rounded-full' />
-                    <div className='size-6 bg-green-700 border border-white rounded-full' />
-                    <div className='size-6 bg-blue-700 border border-white rounded-full' />
+            <button className='relative p-4 bg-white/25 rounded-full  border border-1 bg-white text-xl'>
+                <div className={`hidden absolute space-x-1  p-2 rounded-lg bg-white/25 shadow-md group-hover:flex ${postionClasses[position].tooltip} `}>
+                    {
+                        colors.map((color, index) => (
+                            <div key={index} className={`size-6 ${color} border border-white rounded-full`}></div>
+                        ))
+                    }
 
-                    <div className='absolute left-1/2 transform -translate-x-1/2 -bottom-2 w-0 h-0 border-1-8 border-1-transparent border-r-8 border-r-transparent border-t-8 border-t-white/35'>
+                    <div className={`absolute ${postionClasses[position].arrow}  w-0 h-0 border-1-8 border-1-transparent border-r-8 border-r-transparent border-t-8 border-t-white/35`}>
 
                     </div>
 
                 </div>
-            </button>
+            </button >
         </div >
     )
 }
--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -24,22 +24,19 @@ const Hero = () => {
 
             </div>
 
-            <div className='absolute bottom-40 left-24 group'>
-                <button className='relative p-4 bg-white/25 rounded-full border border-1 bg-white'>
-                    <div className='hidden absolute space-x-1 bottom-full left-1/2 transform -translate-x-1/2 mb-3 p-2 rounded-lg bg-white/25 shadow-md group-hover:flex'>
-                        <div className='size-6 bg-red-700 border border-white rounded-full' />
-                        <div className='size-6 bg-green-700 border border-white rounded-full' />
-                        <div className='size-6 bg-blue-700 border border-white rounded-full' />
 
-                        <div className='absolute left-1/2 transform -translate-x-1/2 -bottom-2 w-0 h-0 border-1-8 border-1-transparent border-r-8 border-r-transparent border-t-8 border-t-white/35'>
-
-                        </div>
-
-                    </div>
-                </button>
-            </div >
 
             <div className='hidden xl:block absolute bottom-40 left-24'>
+                <TooltipButton position="bottom" />
+            </div>
+
+            <div className='hidden xl:block absolute bottom-52 left-96'>
+                <TooltipButton position="bottom" />
+            </div>
+            <div className='hidden xl:block absolute bottom-24 right-[820px]'>
+                <TooltipButton position="bottom" />
+            </div>
+            <div className='hidden xl:block absolute bottom-12 right-16'>
                 <TooltipButton position="bottom" />
             </div>
 
