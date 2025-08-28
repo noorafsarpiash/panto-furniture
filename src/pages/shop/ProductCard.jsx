035b5ecedeb6e205d1b9bd1391590ae4ccaa2607
Noor Piash <noorpiash1234@gmail.com>
2025-08-28 23:57:13 +0600

    shop

--- src/pages/shop/ProductCard.jsx
+++ src/pages/shop/ProductCard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { getImgUrl } from '../../utils/getImageURL'
 
 const ProductCard = ({ product }) => {
     return (
         <div>
             <div className='bg-[#FAFAFA]'>
+                <img src={getImgUrl(`${product.imgUrl}`)} alt="" />
 
             </div>
         </div>
