21a005b5324df5f16a81b5b680614cad0dc03073
Noor Piash <noorpiash1234@gmail.com>
2025-08-31 23:56:09 +0600

    rating update

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
 const Rating = ({ rating }) => {
+    const totalStars = 5;
     return (
-        <div>Rating</div>
+        <div className='flex space-x-1' >Rating</div>
     )
 }
 
